239fd574f0734bcb0bb9e09fea22a676db5868e4
Bokhulenkov <bokhulenkov@gmail.com>
2024-07-22 16:51:43 +0300

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Alexander Bokhulenkov on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
